f0bdc5a484822e7e779c746a7108e87033dc13d5
Marla <93881939+MJZTEC2021@users.noreply.github.com>
2021-12-24 14:48:18 -0500

    Made correction to TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,27 +4,27 @@ function TodoForm({ addTodo }) {
     const [value, setValue] = useState('');
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!value) return;
-        addTodo (value);
-        setValue('');
+      e.preventDefault();
+      if (!value) return;
+      addTodo (value);
+      setValue('');
     };
 
     return(
-        <form className="todo" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="input"
-              placeholder="Enter new to-do"
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-            />
-            <button
-              style={{ backgroundColor: '#0c7cfb' }}
-              onClick={() => handleSubmit}
-            >
-              Submit
-            </button>
+       <form className="todo" onSubmit={handleSubmit}>
+         <input
+           type="text"
+           className="input"
+           placeholder="Enter new to-do"
+           value={value}
+           onChange={(e) => setValue(e.target.value)}
+          />
+          <button
+            style={{ backgroundColor: '#0c7cfb' }}
+            onClick={() => handleSubmit}
+          >
+            Submit
+          </button>
         </form>
     );
 }

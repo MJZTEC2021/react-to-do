6d98a367bb1a9c92162d5422d5a8e5e7221eea2c
Marla <93881939+MJZTEC2021@users.noreply.github.com>
2021-12-24 14:10:26 -0500

    Entering coding for App.js

--- src/App.js
+++ src/App.js
@@ -4,25 +4,47 @@ import './App.css';
 import Todo from "./components/Todo";
 
 function App() {
-  const [todos, setTodos] = React.useState([
+  const [todos, setTodos] = useState([
     { text: "Learn about React" },
     { text: "Meet friend for lunch" },
     { text: "Build really cool todo app" }
   ]);
- 
+  
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text}];
+    setTodos (newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index]
+    .isCompleted;
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
+      <h1>To-Do App</h1>
     <div className="todo-list">
       {todos.map((todo, index) => (
         <Todo
           key={index}
           index={index}
           todo={todo}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
         />
       ))}
+      <TodoForm addTodo={addTodo} />
     </div>
   </div>
-  
   );
 }
 

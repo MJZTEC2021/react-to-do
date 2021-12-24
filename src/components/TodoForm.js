a20236a1465c2aa2bbe670f1ef1461722bb239ae
Marla <93881939+MJZTEC2021@users.noreply.github.com>
2021-12-24 13:25:08 -0500

    Entered coding into the TodoForm.js

---
+++ src/components/TodoForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function TodoForm({ addTodo }) {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo (value);
+        setValue('');
+    };
+
+    return(
+        <form className="todo" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="input"
+              placeholder="Enter new to-do"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <button
+              style={{ backgroundColor: '#0c7cfb' }}
+              onClick={() => handleSubmit}
+            >
+              Submit
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
